64ece0c4c19ea72cff1a23b19a4e0820b695e176
Sebastian <sebastianrme87@gmail.com>
2025-04-29 20:22:07 -0500

    Correccion de errores con fragments sin uso

--- app/src/main/java/com/sena/qfinder/controller/MainActivity.java
+++ app/src/main/java/com/sena/qfinder/controller/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.qfinder.R;
+import com.sena.qfinder.R;
 
 
 public class MainActivity extends AppCompatActivity {
